982572efd585efc9a850f0c53c052e183be6cc61
Pascal Burkhard <pascal.burkhard@gmail.com>
2024-05-14 08:42:46 +0200

    feat: Add a function to relabel categories in a cut column

--- src/geovizir/scales.py
+++ src/geovizir/scales.py
@@ -1,3 +1,5 @@
+from pandas import Series
+
 def label_bins(bins: list[int, float]) -> list[str]:
     """Return a list of labels for a list of bins.
     
@@ -20,4 +22,24 @@ def label_bins(bins: list[int, float]) -> list[str]:
     >>> bins([1,2,3])    
     """
     return [f'< {bins[0]}', *(f'{a}-{b}' for a, b in zip(bins[:-1], bins[1:])), f'> {bins[-1]}']
+
+def relabel_bins(column: Series):
+    """Relabel a column with the bins of the column.
+
+    Parameters
+    ----------
+    column : Series
+        A column to be relabeled.
+
+    Returns
+    -------
+    Series
+        A column with the labels of the bins.
+    """
     
+    categories = column.cat.categories.tolist()
+
+    for cat in categories:
+        column = column.cat.rename_categories({cat: f'{cat.left} - {cat.right}'})
+    
+    return column
--- tests/test_scales.py
+++ tests/test_scales.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas as pd
 from geovizir import scales
 
 class TestScales(unittest.TestCase):
@@ -10,6 +11,24 @@ class TestScales(unittest.TestCase):
         result = scales.label_bins([1,2,3,4])
         expected_result = ['< 1', '1-2', '2-3', '3-4', '> 4']
         self.assertEqual(result, expected_result, "Expected result does not match the actual result")
+
+    def test_relabel_bins(self):
+        series = pd.Series([1, 2, 3, 4, 5, 6], dtype="int")
+        series_cut = pd.cut(series, bins=2)
+        result = scales.relabel_bins(series_cut)
+        # Define the levels in the desired order
+        categories = ['0.995 - 3.5', '3.5 - 6.0']
+
+        # Create the Series with the ordered categories
+        expected_result = pd.Series(
+            pd.Categorical(
+                ['0.995 - 3.5', '0.995 - 3.5', '0.995 - 3.5',
+                '3.5 - 6.0', '3.5 - 6.0', '3.5 - 6.0'],
+                categories=categories,
+                ordered=True
+            )
+        )
+        pd.testing.assert_series_equal(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
